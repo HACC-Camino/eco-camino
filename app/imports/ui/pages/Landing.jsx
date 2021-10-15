8a34c4cfa7cf7fff46c9f1ec9ed627c0b57c1693
Michael <mgainey@hawaii.edu>
2021-10-15 09:46:18 -1000

    added stylesheet for bootstrap

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, GridColumn } from 'semantic-ui-react';
+import Button from 'react-bootstrap/Button';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
+          <GridColumn>
+              <Button size='lg' variant='success'>sup bro</Button>
+          </GridColumn>
         </Grid>
     );
   }
