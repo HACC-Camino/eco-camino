662a72c628f8d1b3624ba5eabddc55177560ed6b
Jerome Gallego <gallego6@hawaii.edu>
2021-10-15 11:54:47 -1000

    initial push

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
-import { Grid, GridColumn } from 'semantic-ui-react';
-import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-          <GridColumn>
-              <Button size='lg' variant='success'>sup bro</Button>
-          </GridColumn>
-        </Grid>
+            <Carousel fade>
+              <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height="600"
+                    src="/images/landing-page/naturetest2.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h2>Build a Community</h2>
+                  <p>Meet with many others who want to also help with the community</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height="600"
+                    src="/images/landing-page/resized-maskedgirl.jpg"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                  <h2>Second slide label</h2>
+                  <p>Clean ups</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height="600"
+                    src="/images/landing-page/naturetest3.jpg"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h2>Third slide label</h2>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
     );
   }
 }
