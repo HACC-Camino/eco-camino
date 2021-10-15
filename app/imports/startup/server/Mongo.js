1e6bce5019b91472f6a2e14ca39716c1dcc71830
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-10-15 11:26:41 -1000

    Created Event Test Data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/StuffCollection.js';
+import { Events } from '../../api/event/EventCollection';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,13 @@ if (Stuffs.count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the collection if empty. */
+if (Events.count() === 0) {
+  if (Meteor.settings.defaultEvent) {
+    console.log('Creating default events.');
+    Meteor.settings.defaultEvent.map(data => Events.define(data));
   }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,8 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/StuffCollection';
+import { Events } from '../../api/event/EventCollection';
 
 /** Publish all the collections you need. */
 Stuffs.publish();
+Events.publish();
 
 /** Need this for the alanning:roles package */
 Meteor.publish(null, function () {
