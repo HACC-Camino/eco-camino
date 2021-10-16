f033e91b20fe4d8dd39fce9add67f3f8a1949055
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-10-15 22:57:06 -0700

    fixed footer- still have to align text

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BsFacebook, BsTwitter, BsLinkedin } from 'react-icons/bs';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '80px' };
+    const rowStyle = { paddingTop: '50px', paddingBottom: '30px' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
-          </div>
+          <Container>
+            <Row style={ divStyle }>
+              <Col > </Col>
+              <Col>
+                <Container>
+                  <Row>
+                    <Col xs={5}>
+                    <BsFacebook size="50px"/>
+                    </Col>
+                    <Col xs={5}>
+                    <BsTwitter size="50px"/>
+                    </Col>
+                    <Col xs={1}>
+                      <BsLinkedin size="50px"/>
+                    </Col>
+                  </Row>
+                  <div className="in-line" style={ rowStyle }>
+                  <Row>
+                    <Col xs={2}>
+                    <a href="/"><h5 >Info</h5></a>
+                    </Col>
+                    <Col xs={2}>
+                    <a href="/"> <h5>Support</h5></a>
+                    </Col>
+                    <Col xs={1}>
+                    <a href="/"> <h5>Partnerships</h5></a>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <div><h5>Terms & Conditions</h5></div>
+                    <div><h5>Privacy Policy</h5></div>
+                  </Row>
+                </div>
+                </Container>
+              </Col>
+              <Col > </Col>
+            </Row>
+          </Container>
         </footer>
     );
   }
