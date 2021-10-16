782dbfbc11ea513f0230c9b14118516738df0601
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-10-15 16:34:21 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -19,11 +19,10 @@ class NavBar1 extends React.Component {
           </Navbar.Brand>
           <Nav className="me-auto">
             {this.props.currentUser ? (
-                [<Nav.Link href="#events" key='events'>Events</Nav.Link>,
+                [<Nav.Link href="#event" key='events'>Events</Nav.Link>,
                   <Nav.Link href="#forums" key='forums'>Forums</Nav.Link>,
                   <Nav.Link href="#profile" key='profile'>Profile</Nav.Link>,
-                  <Nav.Link href="#information" key='information'>Information</Nav.Link>,
-                  <Nav.link href="#event" key='event'>Events</Nav.link>]
+                  <Nav.Link href="#information" key='information'>Information</Nav.Link>]
             ) : ''}
             {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
                 <Nav.Link href="#admin" key='admin'>Admin</Nav.Link>
