9584e0da7945c6fe314a4a2d377969becf756482
Michael <mgainey@hawaii.edu>
2021-10-15 23:22:28 -1000

    finished passing props to profile card

--- app/imports/ui/components/profile/PastEvents.jsx
+++ app/imports/ui/components/profile/PastEvents.jsx
@@ -1,44 +1,39 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
 
-class PastEvents extends React.Component {
-
-    render() {
-        return (
-            <div className='container-lg'>
-                <Table className='table' bordered responsive striped hover>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Event</th>
-                        <th>Time</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Beach Cleanup (Ala Moana)</td>
-                        <td>October 28 2021 9:00 AM</td>
-                    </tr>
-                    </tbody>
-                    <tbody>
-                    <tr>
-                        <td>2</td>
-                        <td>Forest Cleanup (Aiea)</td>
-                        <td>November 3 2021 9:00 AM</td>
-                    </tr>
-                    </tbody>
-                    <tbody>
-                    <tr>
-                        <td>3</td>
-                        <td>Park Cleanup (Ala Moana)</td>
-                        <td>November 15 2021 9:00 AM</td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </div>
-        );
-    }
-}
+const PastEvents = props => (
+    <div className='container-lg'>
+        <Table className='table' bordered responsive striped hover>
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Event</th>
+                <th>Time</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Beach Cleanup (Ala Moana)</td>
+                <td>October 28 2021 9:00 AM</td>
+            </tr>
+            </tbody>
+            <tbody>
+            <tr>
+                <td>2</td>
+                <td>Forest Cleanup (Aiea)</td>
+                <td>November 3 2021 9:00 AM</td>
+            </tr>
+            </tbody>
+            <tbody>
+            <tr>
+                <td>3</td>
+                <td>Park Cleanup (Ala Moana)</td>
+                <td>November 15 2021 9:00 AM</td>
+            </tr>
+            </tbody>
+        </Table>
+    </div>
+);
 
 export default PastEvents;
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,56 +1,64 @@
 import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import { Col, Spinner } from 'react-bootstrap';
 import ProfileCard from '../components/profile/ProfileCard';
 import PlannedEvents from '../components/profile/PlannedEvents';
-import UserOrganizations from '../components/profile/UserOrganizations';
-
 import PastEvents from '../components/profile/PastEvents';
+import { Events } from '../../api/event/EventCollection';
+import { Users } from '../../api/user/UserCollection';
 
-class Profile extends React.Component {
+const Profile = ({ events, ready, userDetail }) => (ready ? (
+    <div className='container' style={{ paddingTop: '30px' }}>
+        <div className='row row-cols-sm-2' style={{ paddingBottom: '30px' }}>
+            <Col sm={4} style={{ paddingBottom: '30px', paddingRight: '30px' }}>
+                <ProfileCard userDetail={userDetail} />
+            </Col>
 
-    render() {
-        return (
-            <div className='container' style={{ paddingTop: '30px' }}>
-                <div className='row row-cols-sm-2' style={{ paddingBottom: '30px' }}>
-                    <div className='col-sm-auto' >
-                        <ProfileCard/>
-                    </div>
-
-                    <div className='col-sm-auto'>
-                        <div className='card'>
-                            <h3 className='card-title' style={{ padding: '10px' }}>My Events</h3>
-                            <PlannedEvents/>
-                        </div>
-                    </div>
-
-                </div>
-                <div className='row row-cols-2'>
-                    <div className='col-4'>
-                        <UserOrganizations/>
-                    </div>
-                    <div className='col-8'>
+            <Col sm={8}>
+                <div className='row' style={{ paddingBottom: '30px' }}>
+                    <div className='card'>
+                        <h3 className='card-title' style={{ padding: '30px' }}>My Events</h3>
                         <PlannedEvents/>
                     </div>
-
-                    <div className='col-lg-8-auto'>
-                        <div className='row' style={{ padding: '30px' }}>
-                            <div className='card'>
-                                <h3 className='card-title' style={{ padding: '10px' }}>My Events</h3>
-                                <PlannedEvents/>
-                            </div>
-                        </div>
-
-                        <div className='row' style={{ padding: '30px' }}>
-                            <div className='card'>
-                                <h3 className='card-title' style={{ padding: '10px' }}>Past Events</h3>
-                                <PastEvents/>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
-            </div>
-        );
-    }
-}
 
-export default Profile;
+                <div className='row'>
+                    <div className='card'>
+                        <h3 className='card-title' style={{ padding: '30px' }}>Past Events</h3>
+                        <PastEvents/>
+                    </div>
+                </div>
+            </Col>
+
+        </div>
+    </div>
+    ) : (
+        <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>
+    )
+);
+
+Profile.propTypes = {
+    events: PropTypes.array.isRequired,
+    userDetail: PropTypes.object,
+    ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+    // Get access to Event documents.
+    const username = Meteor.user()?.username;
+    const ready = Events.subscribeEvent().ready()
+        && Users.subscribeUser().ready()
+        && username !== undefined;
+    const events = Events.getEvenList();
+    const userDetail = Users.getUserDetails(username);
+    return {
+        ready,
+        events,
+        userDetail,
+    };
+})(Profile);
