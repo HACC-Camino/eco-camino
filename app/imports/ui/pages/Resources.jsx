28085634e9ff3badbbfeed04434b41f484deb503
Jerome Gallego <gallego6@hawaii.edu>
2021-10-17 14:04:53 -1000

    added temporary links to organizations

--- app/imports/ui/pages/Resources.jsx
+++ app/imports/ui/pages/Resources.jsx
@@ -6,7 +6,7 @@ const textStyle = { textAlign: 'center' };
 class Resources extends React.Component {
   render() {
     return (
-        <Container className="py-sm-3">
+        <Container className="py-sm-3" id="page-container">
           <Row className="py-sm-3">
             <h1 style={textStyle}>Resources</h1>
           </Row>
@@ -55,41 +55,42 @@ class Resources extends React.Component {
           <h3 style={textStyle}>Organizations</h3>
         </Row>
           <Row>
-            <Col style={textStyle} xs="6" sm="4" >
+            <Col style={textStyle} xs="6" md="4" >
               <Card text="dark" style={{ width: '30rem' }} className="mb-2">
-                <Card.Img variant="top" src="/images/resource-page/hawaiiconservation.jpg" />
+                {/* eslint-disable-next-line max-len */}
+                <Card.Img variant="top" src="/images/resource-page/sustainable_hawaii1.jpg" className="hawaiiConserve" />
                 <Card.Body>
-                  <Card.Title>Hawaii Conservation Alliance</Card.Title>
+                  <Card.Title>Sustainable Coastlines Hawaii</Card.Title>
                   <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the cards content.
                   </Card.Text>
-                  <Button variant="primary" href="https://www.hawaiiconservation.org/">Learn More</Button>
+                  <Button variant="primary" href="https://www.sustainablecoastlineshawaii.org/">Learn More</Button>
                 </Card.Body>
               </Card>
             </Col>
-            <Col style={textStyle} xs="6" sm="4" >
+            <Col style={textStyle} xs="6" md="4" >
               <Card text="dark" style={{ width: '30rem' }} className="mb-2">
-                <Card.Img variant="top" src="holder.js/100px180"/>
+                <Card.Img variant="top" src="/images/resource-page/kupu_logo.jpg" className="hawaiiConserve"/>
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the cards content.
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="primary">Learn More</Button>
                 </Card.Body>
               </Card></Col>
-            <Col style={textStyle} sm="4" >
+            <Col style={textStyle} xs="6" md="4" >
               <Card text="dark" style={{ width: '30rem' }} className="mb-2">
-                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Img variant="top" src="/images/resource-page/trees2_629x661.jpg" />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Trees For Honolulu's Future</Card.Title>
                   <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the cards content.
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="primary">Learn More</Button>
                 </Card.Body>
               </Card></Col>
           </Row>
