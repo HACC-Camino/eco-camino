194a1fc57024af5d589d311baebc9281f91c75f7
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-10-19 01:46:46 -0700

    fixed the navbar

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,7 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 class Landing extends React.Component {
   render() {
     return (
-            <Carousel fade={500}>
+            <Carousel fade >
               <Carousel.Item>
                 <img
                     className="d-block w-100 imageDark"
