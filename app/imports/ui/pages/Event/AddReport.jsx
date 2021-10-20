ecbcbbc08e7541e1a8e6289c0e6611e3bc54acde
Jerome Gallego <gallego6@hawaii.edu>
2021-10-19 19:10:38 -1000

    Points implemented for adding reports

--- app/imports/ui/pages/Event/AddReport.jsx
+++ app/imports/ui/pages/Event/AddReport.jsx
@@ -6,10 +6,14 @@ import swal from 'sweetalert';
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxOption } from '@reach/combobox';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
 import { reportDefineMethod } from '../../../api/report/ReportCollection.methods';
 import UploadPhotoModal from '../../components/aws/UploadPhotoModal';
 import '@reach/combobox/styles.css';
 import mapStyle from '../../components/map/mapStyle';
+import { Users } from '../../../api/user/UserCollection';
+import { userUpdateMethod } from '../../../api/user/UserCollection.methods';
 
 const containerStyle = {
   width: '100%',
@@ -32,7 +36,7 @@ const libraries = ['places'];
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-const AddReport = () => {
+const AddReport = ({ ready1, currentUser }) => {
   const [center, setCenter] = useState({ lat: 21.500, lng: -158.0000 });
   const [zoom, setZoom] = useState(10);
   const panTo = (lat, lng) => {
@@ -105,6 +109,14 @@ const AddReport = () => {
     const accessKey = data;
     const owner = Meteor.user()?.username;
     const description = finalDescription;
+    let updateData = {};
+    updateData = currentUser;
+    updateData.points = currentUser.points + 3;
+    if (ready1) {
+      userUpdateMethod.call(updateData, (error) => (error ?
+          swal('Error', error.message, 'error') :
+          swal('Success', 'Data edited successfully', 'success')));
+    }
     reportDefineMethod.call({
       date, title, location,
       owner, lat, lng, accessKey, description },
@@ -181,4 +193,19 @@ const AddReport = () => {
   );
 };
 
-export default (AddReport);
+AddReport.propTypes = {
+  currentUser: PropTypes.object,
+  ready: PropTypes.bool,
+};
+
+export default withTracker(() => {
+  const username = Meteor.user()?.username;
+  const ready1 = Users.subscribeUser().ready();
+  const currentUser = Users.getUserDetails(username);
+  console.log(currentUser);
+  console.log(username);
+  return {
+    currentUser,
+    ready1,
+  };
+})(AddReport);
